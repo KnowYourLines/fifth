ed4ae9cb493de754c7d199a59d714d9ef62607ba
Johnny <knowyourlines@outlook.com>
2022-03-22 09:23:53 +0000

    added push command

--- stack.py
+++ stack.py
@@ -1,14 +1,21 @@
 def fifth():
     stack = []
     end = False
-    valid_commands = ['END']
+    valid_commands = ['END', 'PUSH']
     while not end:
         print(f"stack is {stack}")
         cmd = input("")
-        if cmd not in valid_commands:
+        if cmd.split(' ')[0] not in valid_commands:
             print('ERROR')
         if cmd == "END":
             end = True
+        if cmd.split(' ')[0] == 'PUSH':
+            if len(cmd.split(' ')) > 2:
+                print('ERROR')
+            try:
+                stack.append(int(cmd.split(' ')[1]))
+            except ValueError:
+                print('ERROR')
 
 
 if __name__ == '__main__':
--- test_stack.py
+++ test_stack.py
@@ -18,6 +18,18 @@ class FifthTestCase(unittest.TestCase):
         fifth()
         self.assertEqual(stdout.getvalue(), 'stack is []\nERROR\nstack is []\n')
 
+    @patch('builtins.input', side_effect=["PUSH 3", "END"])
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_push_to_stack(self, stdout, mock_input):
+        fifth()
+        self.assertEqual(stdout.getvalue(), 'stack is []\nstack is [3]\n')
+
+    @patch('builtins.input', side_effect=["PUSH 3.2", "END"])
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_push_must_be_integer(self, stdout, mock_input):
+        fifth()
+        self.assertEqual(stdout.getvalue(), 'stack is []\nERROR\nstack is []\n')
+
 
 if __name__ == '__main__':
     unittest.main()
